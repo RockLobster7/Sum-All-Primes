02e2f33e6552e24ba684abb130144ec2c96a0455
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-26 20:36:23 +1000

    completed challenge

--- Sum-All-Primes.js
+++ Sum-All-Primes.js
@@ -7,10 +7,26 @@ A prime number is defined as a number greater than one and having only two divis
 The provided number may not be a prime.
 */
 
-function sumPrimes(num) {
-    return num;
-  }
 
-  console.log(sumPrimes(10)); // should return a number.
-  console.log(sumPrimes(10)); // should return 17.
-  console.log(sumPrimes(977)); // should return 73156.
+function isPrime(num) {
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) return false;
+    }
+    return num > 1;
+}
+
+function sumPrimes(num) {
+    var result = 0;
+    do {
+        if (isPrime(num)) result += num;
+        num--;
+    } while (num > 0);
+
+    return result;
+}
+
+
+
+console.log(sumPrimes(10)); // should return a number.
+console.log(sumPrimes(10)); // should return 17.
+console.log(sumPrimes(977)); // should return 73156.
